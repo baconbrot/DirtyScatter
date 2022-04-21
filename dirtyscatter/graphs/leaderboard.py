73b32e97c884ea2ed9f4a071fb83296d29db3833
Christian Ruf <c.ruf99@gmail.com>
2022-04-21 20:08:33 +0200

    Fix zero-data bug

--- dirtyscatter/graphs/leaderboard.py
+++ dirtyscatter/graphs/leaderboard.py
@@ -14,33 +14,31 @@ log = logging.getLogger(__name__)
 def generate_plot():
     #plt.style.use('dark_background')
     fig, ax = plt.subplots()
-    try:
-        oldest = History.get_oldest()[0]
-        begin = max(int(time()) - 30 * 24 * 3600, oldest.timestamp)
-    except:
-        begin = int(time())
+    begin = int(time()) - 30 * 24 * 3600
     histories = History.get_after_timestamp(begin)
     user_histories = {}
-    for history in histories:
+    # Group by username
+    for user_history in histories:
         try:
-            user_histories[history.name] = user_histories[history.name]+[history]
-        except:
-            user_histories[history.name] = [history]
-    if len(user_histories.keys()) == 0:
-        top_users = User.get_top(10)
-        for user in top_users:
-            user_histories[user.name] = [History.History(name=user.name, scatter=user.scatter, timestamp=int(time())),
-                                         History.History(name=user.name, scatter=user.scatter, timestamp=int(time()))]
-    for user in user_histories.keys():
-        user_history = user_histories[user]
-        user_history.append(History.History(name=user, scatter=user_history[-1].scatter, timestamp=int(time())))
+            user_histories[user_history.name] = user_histories[user_history.name]+[user_history]
+        except KeyError:
+            user_histories[user_history.name] = [user_history]
+    # Add user graph to plot
+    for user in User.get_all()[:32]:
+        name = user.name
+        user_history = []
+        try:
+            user_history = user_histories[name]
+        except KeyError:
+            History.insert_history(History.History(name=name, scatter=user.scatter, timestamp=int(time())))
+        user_history.append(History.History(name=name, scatter=user.scatter, timestamp=int(time()+1)))
         x = [dt.datetime.fromtimestamp(history.timestamp) for history in user_history]
         y = [history.scatter for history in user_history]
-        ax.step(x, y, where='post', label=f'{user}')
+        ax.step(x, y, where='post', label=f'{user.name}')
     xfmt = md.DateFormatter('%d.%m')
     ax.set_ylabel('Scatter')
     ax.xaxis.set_major_formatter(xfmt)
-    ax.legend(fontsize=7, labelspacing=0.15, bbox_to_anchor=(1, 1), ncol=2)
+    ax.legend(fontsize=7, labelspacing=0.15, bbox_to_anchor=(1, 1), ncol=1)
     plt.savefig("output.png", bbox_inches="tight")
     log.debug(f'Saved leadboard-graph to output.png')
     plt.close(fig)
